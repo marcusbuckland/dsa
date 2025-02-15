797535f64a987a25074b1564a733bc4218409549
marcusbuckland <alreadysignedin@gmail.com>
2025-02-15 13:57:30 +1300

    fix: typo in dll tests

--- tests/test_doubly_linked_list.py
+++ tests/test_doubly_linked_list.py
@@ -1,116 +1,116 @@
 from src.DoublyLinkedList import DoublyLinkedList
 
 def test_constuctor_01():
-    ddll = DoublyLinkedList()
-    assert str(ddll) == ""
+    dll = DoublyLinkedList()
+    assert str(dll) == ""
 
 def test_constuctor_02():
-    ddll = DoublyLinkedList('A')
-    assert str(ddll) == ('(A)')
+    dll = DoublyLinkedList('A')
+    assert str(dll) == ('(A)')
 
 def test_str_01():
-    ddll = DoublyLinkedList()
-    assert str(ddll) is ""
+    dll = DoublyLinkedList()
+    assert str(dll) is ""
 
 def test_str_02():
-    ddll = DoublyLinkedList('ABC')
-    assert str(ddll) == "(ABC)"
+    dll = DoublyLinkedList('ABC')
+    assert str(dll) == "(ABC)"
 
 def test_str_03():
-    ddll = DoublyLinkedList(1)
-    ddll.append(2)
-    assert str(ddll) == "(1)<->(2)"
+    dll = DoublyLinkedList(1)
+    dll.append(2)
+    assert str(dll) == "(1)<->(2)"
 
 def test_append_01():
-    ddll = DoublyLinkedList()
-    ddll.append(1)
-    assert ddll.head.value == 1
-    assert ddll.tail.value == 1
-    assert ddll.head.next is None
-    assert ddll.tail.next is None
-    assert ddll.head.prev is None
-    assert ddll.head.prev is None
-    assert ddll.length == 1
+    dll = DoublyLinkedList()
+    dll.append(1)
+    assert dll.head.value == 1
+    assert dll.tail.value == 1
+    assert dll.head.next is None
+    assert dll.tail.next is None
+    assert dll.head.prev is None
+    assert dll.head.prev is None
+    assert dll.length == 1
 
 def test_append_02():
-    ddll = DoublyLinkedList()
-    ddll.append(1)
-    ddll.append(2)
-    assert ddll.head.value == 1
-    assert ddll.head.next.value == 2
-    assert ddll.head.prev is None
-    assert ddll.tail.value == 2
-    assert ddll.tail.next is None
-    assert ddll.tail.prev.value == 1
-    assert ddll.length == 2
+    dll = DoublyLinkedList()
+    dll.append(1)
+    dll.append(2)
+    assert dll.head.value == 1
+    assert dll.head.next.value == 2
+    assert dll.head.prev is None
+    assert dll.tail.value == 2
+    assert dll.tail.next is None
+    assert dll.tail.prev.value == 1
+    assert dll.length == 2
 
 def test_append_03():
-    ddll = DoublyLinkedList()
+    dll = DoublyLinkedList()
     for i in range(1, 10):
-        ddll.append(i)
-    assert ddll.head.value == 1
-    assert ddll.head.next.value == 2
-    assert ddll.head.prev is None
-    assert ddll.tail.value == 9
-    assert ddll.tail.next is None
-    assert ddll.tail.prev.value is 8
-    assert ddll.length == 9
+        dll.append(i)
+    assert dll.head.value == 1
+    assert dll.head.next.value == 2
+    assert dll.head.prev is None
+    assert dll.tail.value == 9
+    assert dll.tail.next is None
+    assert dll.tail.prev.value is 8
+    assert dll.length == 9
 
 def test_pop_01():
-    ddll = DoublyLinkedList()
-    assert ddll.length == 0
-    assert ddll.pop() is None
-    assert ddll.pop() is None
-    assert ddll.length == 0
+    dll = DoublyLinkedList()
+    assert dll.length == 0
+    assert dll.pop() is None
+    assert dll.pop() is None
+    assert dll.length == 0
 
 
 def test_pop_02():
-    ddll = DoublyLinkedList()
-    ddll.append(1)
-    node = ddll.pop()
-    assert ddll.head is None
-    assert ddll.tail is None
-    assert ddll.length == 0
+    dll = DoublyLinkedList()
+    dll.append(1)
+    node = dll.pop()
+    assert dll.head is None
+    assert dll.tail is None
+    assert dll.length == 0
     assert node.value == 1
 
 def test_pop_03():
-    ddll = DoublyLinkedList()
+    dll = DoublyLinkedList()
     for i in range(1, 10):
-        ddll.append(i)
-    node = ddll.pop() # 9
+        dll.append(i)
+    node = dll.pop() # 9
     assert node.value == 9
 
-    assert ddll.head.value == 1
-    assert ddll.head.prev is None
-    assert ddll.head.next.value == 2
-    assert ddll.tail.value == 8
-    assert ddll.tail.prev.value == 7
-    assert ddll.tail.next is None
-    assert ddll.length == 8
+    assert dll.head.value == 1
+    assert dll.head.prev is None
+    assert dll.head.next.value == 2
+    assert dll.tail.value == 8
+    assert dll.tail.prev.value == 7
+    assert dll.tail.next is None
+    assert dll.length == 8
 
-    node = ddll.pop() # 8
+    node = dll.pop() # 8
     assert node.value == 8
-    assert ddll.length == 7
+    assert dll.length == 7
 
-    node = ddll.pop() # 7
+    node = dll.pop() # 7
     assert node.value == 7
-    assert ddll.length == 6
+    assert dll.length == 6
 
-    node = ddll.pop() # 6
+    node = dll.pop() # 6
     assert node.value == 6
-    assert ddll.length == 5
+    assert dll.length == 5
 
-    node = ddll.pop() # 5
+    node = dll.pop() # 5
     assert node.value == 5
-    assert ddll.length == 4
+    assert dll.length == 4
 
-    assert ddll.head.value == 1
-    assert ddll.head.prev is None
-    assert ddll.head.next.value == 2
-    assert ddll.tail.value == 4
-    assert ddll.tail.next is None
-    assert ddll.tail.prev.value is 3
-    assert ddll.length == 4
+    assert dll.head.value == 1
+    assert dll.head.prev is None
+    assert dll.head.next.value == 2
+    assert dll.tail.value == 4
+    assert dll.tail.next is None
+    assert dll.tail.prev.value is 3
+    assert dll.length == 4
 
 def test_prepend_01():
     dll = DoublyLinkedList()
